09130f4cfe310a85de42c461b55c2bcd6601980f
bhumiGautam <gautamkumarpandey149@gmail.com>
2024-05-19 16:51:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Social Media Influencer","Youtuber","Content Manager"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
